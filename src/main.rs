7bf4c1b1d2b10107ffb06405d47f63c7e93e0938
lingjiatong <ljtnono@163.com>
2026-01-03 23:36:42 +0800

    feature:新增.gitignore文件

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+
+fn mcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
+
+#[test]
+fn test_gcd () {
+    assert_eq!(mcd(14, 15), 1);
+}
