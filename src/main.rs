b5a5c66fa17e629adfb7a146fbfd6d84cb2dc1b9
lingjiatong <ljtnono@163.com>
2026-01-05 23:34:25 +0800

    faeture:循环

--- src/main.rs
+++ src/main.rs
@@ -235,12 +235,136 @@
 
 // ==================== capter05 ==================== //
 
-fn main() {
-    let number = 3;
+// fn main() {
+//     let number = 3;
+//
+//     if number < 5 {
+//         println!("condition was true");
+//     } else {
+//         println!("condition was false");
+//     }
+// }
 
-    if number < 5 {
-        println!("condition was true");
-    } else {
-        println!("condition was false");
+
+// fn main() {
+//
+//
+//     let number = 3;
+//
+//     if number == 3 {
+//         println!("number was three");
+//     }
+//
+// }
+
+
+// fn main() {
+//
+//     let number = 6;
+//
+//     if number % 4 == 0 {
+//         println!("{} is divisible by 4", number);
+//     } else if number % 3 == 0 {
+//         println!("{} is divisible by 3", number);
+//     } else if number % 2 == 0 {
+//         println!("{} is divisible by 2", number);
+//     } else {
+//         println!("{} is not divisible by 4, 3, or 2", number);
+//     }
+//
+// }
+
+// fn main() {
+//     let condition = true;
+//
+//     let number = if condition { 5 } else { 6 };
+//
+//     println!("{}", number);
+//
+// }
+
+// fn main() {
+//
+//     let mut counter = 0;
+//
+//     // 循环返回值
+//     let result = loop {
+//         counter += 1;
+//
+//         if counter == 10 {
+//             // break 后的一个表达式可以作为返回值
+//             break counter * 2;
+//         }
+//     };
+//
+//     println!("The result is {}", result);
+// }
+
+// fn main() {
+//     let mut count = 0;
+//
+//     'counting_up: loop {
+//         println!("count = {count}");
+//
+//         let mut remaining = 10;
+//
+//         loop {
+//             println!("remaining = {}", remaining);
+//             if remaining == 9 {
+//                 break;
+//             }
+//             if count == 2 {
+//                 break 'counting_up;
+//             }
+//             remaining -= 1;
+//         }
+//
+//         count += 1;
+//     }
+//     println!("End count = {count}");
+// }
+
+// fn main() {
+//     let mut number = 3;
+//
+//     while number != 0 {
+//         println!("{number}");
+//
+//         number -= 1;
+//     }
+//
+//     println!("LIFTOFF!!!");
+// }
+
+// fn main() {
+//     let a = [10 ,20, 30, 40, 50];
+//
+//     let mut index = 0;
+//
+//     while index < 5 {
+//         println!("the value is: {}", a[index]);
+//         index += 1;
+//     }
+//
+// }
+
+// fn main() {
+//
+//     let a = [10, 20, 30, 40, 50];
+//
+//     for e in a {
+//         print!("the value is {} ", e);
+//     }
+//
+// }
+
+fn main() {
+
+    for number in (1..4).rev() {
+        println!("{}", number);
     }
+    println!("LIFTOFF!!!")
+
 }
+
+
